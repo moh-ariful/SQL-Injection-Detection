66eba2cf7843134e684ad412462c53e75e8a39e1
Moh Ariful <106416095+moh-ariful@users.noreply.github.com>
2023-01-29 15:13:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,23 @@
 import requests
 import re
 
-
+# URL website yang akan diperiksa
 url = "https://django.my.id"
 
-keywords = ["sql", "Syntax", "Error", "Drop", "Union", "Where", "Or"]
+# Kata kunci yang akan dicari dalam respon website
+keywords = ["SQL", "syntax", "error", "Drop", "Union", "Where", "Select"]
 
+# Mengirim request GET ke website
 response = requests.get(url)
 
+# Mengambil teks dari respon
 text = response.text
 
+# Mencari kata kunci dalam teks respon
 for keyword in keywords:
     if re.search(keyword, text):
-        print("Website lemah terhadap SQL Injection")
+        print("Website rentan terhadap SQL injection!")
         break
-
 else:
-    print("Website tidak terdeteksi lemah terhadap serangan SQL Injection")
+    print("Website tidak terdeteksi rentan terhadap SQL injection.")
+
